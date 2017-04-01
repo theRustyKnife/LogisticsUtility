e29a6272311c16b4a4d20c5731beb09fc662ff30
theRustyKnife <therustyknife13@gmail.com>
2017-04-01 16:50:34 +0200

    Fixed compatibility with Reactors

--- LogisticsSigns/prototypes/styles.lua
+++ LogisticsSigns/prototypes/styles.lua
@@ -1,62 +1,72 @@
 local icons = {}
 for i, icon in pairs(ICONS) do
-  for _, object in pairs(data.raw[icon]) do
-    table.insert(icons, object.name)
-    data.raw["gui-style"].default["icon-notice-" .. object.name] =
-    {
-        type = "button_style",
-        parent = "button_style",
-        width = 32,
-        height = 32,
-        top_padding = 1,
-        right_padding = 1,
-        bottom_padding = 1,
-        left_padding = 1,
-        font = "default-button",
-        default_font_color = {r=0.92, g=0.92, b=0.92},
-        default_graphical_set =
-        {
-            type = "monolith",
-            monolith_image =
-            {
-                filename = object.icon,
-                priority = "extra-high-no-scale",
-                width = 32,
-                height = 32,
-                x = 0
-            }
-        },
-        hovered_graphical_set =
-        {
-            type = "monolith",
-            monolith_image =
-            {
-                filename = object.icon,
-                priority = "extra-high-no-scale",
-                width = 32,
-                height = 32,
-                x = 0
-            }
-        },
-        clicked_graphical_set =
-        {
-            type = "monolith",
-            monolith_image =
-            {
-                filename = object.icon,
-                width = 32,
-                height = 32,
-                x = 0
-            }
-        }
-    }
-  end
+	for _, object in pairs(data.raw[icon]) do
+		local filename = object.icon
+		if not filename and object.icons then
+			for _, v in pairs(object.icons) do
+				filename = v.icon
+				break
+			end
+		end
+		
+		if filename then
+			table.insert(icons, object.name)
+			data.raw["gui-style"].default["icon-notice-" .. object.name] =
+			{
+				type = "button_style",
+				parent = "button_style",
+				width = 32,
+				height = 32,
+				top_padding = 1,
+				right_padding = 1,
+				bottom_padding = 1,
+				left_padding = 1,
+				font = "default-button",
+				default_font_color = {r=0.92, g=0.92, b=0.92},
+				default_graphical_set =
+				{
+					type = "monolith",
+					monolith_image =
+					{
+						filename = filename,
+						priority = "extra-high-no-scale",
+						width = 32,
+						height = 32,
+						x = 0
+					}
+				},
+				hovered_graphical_set =
+				{
+					type = "monolith",
+					monolith_image =
+					{
+						filename = filename,
+						priority = "extra-high-no-scale",
+						width = 32,
+						height = 32,
+						x = 0
+					}
+				},
+				clicked_graphical_set =
+				{
+					type = "monolith",
+					monolith_image =
+					{
+						filename = filename,
+						width = 32,
+						height = 32,
+						x = 0
+					}
+				}
+			}
+		end
+	end
 end
 
 data:extend({{
-    type = "flying-text",
-    name = "SIGNPOST_ICONS",
-    time_to_live = 0,
-    speed = 1,
-    order = serpent.dump(icons)
+	type = "flying-text",
+	name = "SIGNPOST_ICONS",
+	time_to_live = 0,
+	speed = 1,
+	order = serpent.dump(icons)
 }})
